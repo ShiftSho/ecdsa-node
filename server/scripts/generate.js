29e5d60305128bd6910e0749076a7fd59c673b38
ShiftSho <jlongprivate@gmail.com>
2023-03-05 23:34:24 -0500

    updated

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -6,10 +6,10 @@ const privateKey = secp.utils.randomPrivateKey();
 
 console.log('private key:', toHex(privateKey));
 
-const pubKey = secp.getPublicKey(privateKey);
+const publicKey = secp.getPublicKey(privateKey);
 
-console.log('public key:', toHex(pubKey));
+console.log('public key:', toHex(publicKey));
 
-const ethAddress = keccak256(pub.slice(1)).slice(-20);
+const ethAddress = keccak256(publicKey.slice(1)).slice(-20);
 
 console.log('eth address:', "0x" + toHex(ethAddress));
